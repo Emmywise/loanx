6a58ace5da8c81aef5043468a2bbfbaaa0c3cc30
Leke-Ariyo <lexmill99@gmail.com>
2020-04-30 10:01:27 +0100

    completion and testing of cash flow accumulated, monthly as well as initialization of cash flow projected

--- accounting/views.py
+++ accounting/views.py
@@ -1,7 +1,10 @@
-#import datetime
+import datetime
+import calendar
+
 from datetime import datetime
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from accounts.models import Branch
 from accounting.models import CashFlow
 
@@ -19,19 +22,68 @@ class CashFlowAccumlated(APIView):
     def get(self, request, pk=None):
         start_date = request.GET.get("start_date")
         end_date = request.GET.get("end_date")
-        #date_str = '09-19-2018'
-        start_object = datetime.strptime(start_date, '%m-%d-%Y').date()    
-        end_object = datetime.strptime(end_date, '%m-%d-%Y').date()
-        first_cash_flow = CashFlow.objects.filter(date=start_object)
-        last_cash_flow = CashFlow.objects.filter(date=end_object)     
-        print(first_cash_flow)
-        print("leke")
-        print(last_cash_flow) 
-        # filtered_loans = Loan.objects.filter(maturity_date__lte = datetime.date.today())
-        # data = []
-        # for filtered_loan in filtered_loans:
-        #     if ((datetime.date.today() - filtered_loans[0].maturity_date).days) >= int(days_due):
-        #         data.append(filtered_loan)
-        #serializer = LoanSerializer(data, many=True)
-        #return Response(serializer.data)
-        #return "Leke"
+        branch = request.GET.get("branch")
+        try:
+            #http://127.0.0.1:8000/api/cash_flow_accumulated/?start_date=09-27-2020&&end_date=09-28-2020&&branch?1
+            start_object = datetime.strptime(start_date, '%m-%d-%Y').date()    
+            end_object = datetime.strptime(end_date, '%m-%d-%Y').date()
+            first_cash_flow = CashFlow.objects.filter(branch=branch).filter(date=start_object)[0] 
+            last_cash_flow = CashFlow.objects.filter(branch=branch).filter(date=end_object)[0]  
+            branch_capital = first_cash_flow.branch_capital
+            expenses = last_cash_flow.expenses - first_cash_flow.expenses
+            payroll = last_cash_flow.payroll - first_cash_flow.payroll
+            loan_released = last_cash_flow.loan_released - first_cash_flow.loan_released
+            loan_repayment = last_cash_flow.loan_repayment - first_cash_flow.loan_repayment
+            deposit = last_cash_flow.deposit - first_cash_flow.deposit
+            withdrawal = last_cash_flow.withdrawal - first_cash_flow.withdrawal
+            date = str(start_date) + " " + "to" + " " + str(end_date)
+            result = {"branch": branch, "branch_capital": branch_capital, "expenses": expenses, "payroll": payroll, "loan_released": loan_released, "loan_repayment": loan_repayment, "deposit": deposit, "withdrawal": withdrawal, "date": date}
+            return Response(result)
+        except:
+            return Response("Your input is not valid")
+
+
+class CashFlowMonthly(APIView):
+    def get(self, request, pk=None):
+        branch = request.GET.get("branch")
+        month = request.GET.get("month")
+        year = request.GET.get("year")
+
+        helper = calendar.monthrange(int(year), int(month))
+        last_day = helper[1]
+        if len(month) == 1:
+            month = "0" + month
+
+        start_date = month+"-"+"01"+"-"+year
+        end_date = month+"-"+str(last_day)+"-"+year
+        print(start_date)
+        print(end_date)
+        try:
+            #http://127.0.0.1:8000/api/cash_flow_monthly/?month=4&year=2020&branch=1
+            start_object = datetime.strptime(start_date, '%m-%d-%Y').date()    
+            end_object = datetime.strptime(end_date, '%m-%d-%Y').date()
+            first_cash_flow = CashFlow.objects.filter(branch=branch).filter(date=start_object)[0] 
+            last_cash_flow = CashFlow.objects.filter(branch=branch).filter(date=end_object)[0]  
+            branch_capital = first_cash_flow.branch_capital
+            expenses = last_cash_flow.expenses - first_cash_flow.expenses
+            payroll = last_cash_flow.payroll - first_cash_flow.payroll
+            loan_released = last_cash_flow.loan_released - first_cash_flow.loan_released
+            loan_repayment = last_cash_flow.loan_repayment - first_cash_flow.loan_repayment
+            deposit = last_cash_flow.deposit - first_cash_flow.deposit
+            withdrawal = last_cash_flow.withdrawal - first_cash_flow.withdrawal
+            date = str(start_date) + " " + "to" + " " + str(end_date)
+            result = {"branch": branch, "branch_capital": branch_capital, "expenses": expenses, "payroll": payroll, "loan_released": loan_released, "loan_repayment": loan_repayment, "deposit": deposit, "withdrawal": withdrawal, "date": date}
+            return Response(result)
+        except:
+            return Response("Your input is not valid")
+
+
+class CashFlowAccumlated(APIView):
+    def get(self, request, pk=None):
+        start_date = request.GET.get("start_date")
+        end_date = request.GET.get("end_date")
+        branch = request.GET.get("branch")
+        try:
+            return Response("result")
+        except:
+            return "Your input is not valid"
